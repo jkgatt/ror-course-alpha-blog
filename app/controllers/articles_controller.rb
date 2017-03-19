7663dac544aeb47fc4fc02880431dfc3278bd98f
Jean Gatt <jeangatt@gmail.com>
2017-03-19 16:28:25 +0100

    Added partials and destory functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,6 +38,13 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		flash[:notice] = "Article was sucessfully deleted/destroyed"
+		redirect_to articles_path
+	end
+
 	private
 		# Requires the article param object and permits the data into the object
 		def article_params
