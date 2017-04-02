40d40dcf5d6646b1ea5a01fa7c0a8d6e944c598f
Jean Gatt <jeangatt@gmail.com>
2017-04-02 11:17:31 +0200

    Added basic authentication to the app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
 
-	before_action :set_article, only: [:edit,:show,:update,:destroy]
+	before_action :set_article, only: [:edit,:show,:update, :destroy]
+	before_action :require_user, except: [:index, :show]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
 
 	def index
 		@articles = Article.paginate(page: params[:page], per_page: 5)
@@ -14,8 +16,7 @@ class ArticlesController < ApplicationController
 		# render plain: params[:article].inspect
 		@article = Article.new(article_params)
 
-		# Quick fix just for now!
-		@article.user= User.first
+		@article = current_user
 		if @article.save
 			#Flashes a message, saved for just once
 			flash[:success] = "The article was successfully created"
@@ -54,5 +55,12 @@ class ArticlesController < ApplicationController
 
 		def set_article()
 			@article = Article.find(params[:id])
+		end
+
+		def require_same_user
+			if current_user != @article.user
+				flash[:danger] = "You can only edit or delete your own articles"
+				redirect_to root_path
+			end
 		end
 end
